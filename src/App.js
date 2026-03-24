32fab58a42aeb50e301457b4a1538dc4f1cab288
oumeyma-elaammari <oumeyma.elaammari.ensao@ump.ac.ma>
2026-03-24 23:48:07 +0100

    update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ import Certificates from './components/Certificates';
 import Portfolio from './components/Portfolio';
 import Services from './components/Services';
 import Contact from './components/Contact';
+import Quote from './components/Quote';
 import Footer from './components/Footer';
+
 
 function App() {
   const [showScrollTop, setShowScrollTop] = useState(false);
@@ -54,6 +56,7 @@ function App() {
         <Portfolio />
         <Services />
         <Contact />
+        <Quote />
       </main>
       <Footer />
       
